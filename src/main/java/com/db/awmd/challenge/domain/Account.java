d078119da121bab1137133b682d5ae76fa1171a9
Manish Bhagat <manish.j.bhagat@gmail.com>
2020-06-13 09:15:21 +0530

    ThreadSafe and Deadlock mitigation

--- src/main/java/com/db/awmd/challenge/domain/Account.java
+++ src/main/java/com/db/awmd/challenge/domain/Account.java
@@ -1,12 +1,15 @@
 package com.db.awmd.challenge.domain;
 
+import com.db.awmd.challenge.exception.InsufficientBalanceException;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.math.BigDecimal;
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
-import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Data
@@ -19,6 +22,9 @@ public class Account {
   @NotNull
   @Min(value = 0, message = "Initial balance must be positive.")
   private BigDecimal balance;
+
+  private final Lock lock = new ReentrantLock();
+  private final Random number = new Random(123L);
 
   public Account(String accountId) {
     this.accountId = accountId;
@@ -42,5 +48,32 @@ public class Account {
 
   public void setBalance(BigDecimal balance) {
     this.balance = balance;
+  }
+
+  public synchronized void depositAmount(Account debitAccount, BigDecimal amount) throws InsufficientBalanceException, InterruptedException {
+    while(true){
+      if(this.lock.tryLock()){
+        try {
+          if (debitAccount.lock.tryLock()) {
+            try {
+              if (debitAccount.getBalance().compareTo(amount) < 0) {
+                throw new InsufficientBalanceException(" Insufficient Balance");
+              }
+              debitAccount.getBalance().subtract(amount);
+              setBalance(this.balance.add(amount));
+              break;
+            } finally {
+              debitAccount.lock.unlock();
+            }
+          }
+        } finally {
+          this.lock.unlock();
+        }
+        int n = number.nextInt(1000);
+        int TIME = 1000 + n; // 1 second + random delay to prevent livelock
+        Thread.sleep(TIME);
+      }
+    }
+
   }
 }
--- src/main/java/com/db/awmd/challenge/repository/AccountsRepositoryInMemory.java
+++ src/main/java/com/db/awmd/challenge/repository/AccountsRepositoryInMemory.java
@@ -1,7 +1,6 @@
 package com.db.awmd.challenge.repository;
 
 import com.db.awmd.challenge.domain.Account;
-import com.db.awmd.challenge.domain.Transaction;
 import com.db.awmd.challenge.exception.DuplicateAccountIdException;
 import com.db.awmd.challenge.exception.InsufficientBalanceException;
 import com.db.awmd.challenge.service.NotificationService;
@@ -18,91 +17,76 @@ import java.util.concurrent.ConcurrentHashMap;
 
 
 @Repository
-public class AccountsRepositoryInMemory implements AccountsRepository , NotificationService {
+public class AccountsRepositoryInMemory implements AccountsRepository, NotificationService {
 
-  public static Logger log = LoggerFactory.getLogger(AccountsRepositoryInMemory.class);
-  private final Map<String, Account> accounts = new ConcurrentHashMap<>();
+    public static Logger log = LoggerFactory.getLogger(AccountsRepositoryInMemory.class);
+    private final Map<String, Account> accounts = new ConcurrentHashMap<>();
 
-  @Override
-  public void createAccount(Account account) throws DuplicateAccountIdException {
-    Account previousAccount = accounts.putIfAbsent(account.getAccountId(), account);
-    if (previousAccount != null) {
-      throw new DuplicateAccountIdException(
-        "Account id " + account.getAccountId() + " already exists!");
-    }
-  }
-
-  @Override
-  public Account getAccount(String accountId) {
-    return accounts.get(accountId);
-  }
-
-  @Override
-  public void clearAccounts() {
-    accounts.clear();
-  }
-
-  @Override
-  public String transferAccount(String fromAccount, String toAccount, BigDecimal amount) throws InsufficientBalanceException {
-
-    Account debitAccount = this.getAccount(fromAccount);
-    Account creditAccount = this.getAccount(toAccount);
-
-    //Checking if source have sufficient balance
-    if(debitAccount.getBalance().compareTo(amount) < 0){
-      log.error(String.format("Account with %s has insufficient balance for transfer",debitAccount.getAccountId()));
-      throw new InsufficientBalanceException(String.format("Account with %s has insufficient balance for transfer",debitAccount.getAccountId()));
+    @Override
+    public void createAccount(Account account) throws DuplicateAccountIdException {
+        Account previousAccount = accounts.putIfAbsent(account.getAccountId(), account);
+        if (previousAccount != null) {
+            throw new DuplicateAccountIdException(
+                    "Account id " + account.getAccountId() + " already exists!");
+        }
     }
 
-    Long transactionId = generateUniqueTransactionId();
-
-    // Debit Transaction Record.
-    Transaction debitTansaction =new Transaction();
-    debitTansaction.setTransactionId(transactionId);
-    debitTansaction.setTransactionTimestamp();
-    debitTansaction.setTransactionAmount(amount);
-    debitTansaction.setTransactionType("Debit");
-
-    // Credit Transaction Record
-    Transaction creditTransaction = new Transaction();
-    creditTransaction.setTransactionId(transactionId);
-    creditTransaction.setTransactionType("Credit");
-    creditTransaction.setTransactionTimestamp();
-    creditTransaction.setTransactionAmount(amount);
-
-    //Debit from Debitor Account
-    debitAccount.setBalance(debitAccount.getBalance().subtract(amount));
-    //Credit to Creditor Account
-    creditAccount.setBalance(creditAccount.getBalance().add(amount));
-
-    //Send Notifications
-    notifyAboutTransfer(debitAccount,debitTansaction.toString());
-    notifyAboutTransfer(creditAccount,creditTransaction.toString());
-
-    log.info("Money Transfer Process completed Successfully");
-    return String.format("Debit Transaction Details: %s Successful",debitTansaction.getTransactionId());
-  }
-
-
-  private Long generateUniqueTransactionId()
-  {
-    long val;
-    do
-    {
-      final UUID uid = UUID.randomUUID();
-      final ByteBuffer buffer = ByteBuffer.wrap(new byte[16]);
-      buffer.putLong(uid.getLeastSignificantBits());
-      buffer.putLong(uid.getMostSignificantBits());
-      final BigInteger bi = new BigInteger(buffer.array());
-      val = bi.longValue();
+    @Override
+    public Account getAccount(String accountId) {
+        return accounts.get(accountId);
     }
-    // making sure that the ID is in positive space, if its not simply repeat the process
-    while (val < 0);
-    return val;
-  }
 
-  @Override
-  public void notifyAboutTransfer(Account account, String transferDescription) {
-    log.info(String.format("TransferDescription for %s : %s",account,transferDescription));
-  }
+    @Override
+    public void clearAccounts() {
+        accounts.clear();
+    }
+
+    @Override
+    public synchronized String transferAccount(String fromAccount, String toAccount, BigDecimal amount) throws InsufficientBalanceException {
+
+        Account debitAccount = this.getAccount(fromAccount);
+        Account creditAccount = this.getAccount(toAccount);
+
+        log.info(String.format("Initiating Transfer from %s to %s of amount %s", fromAccount, toAccount, amount.toString()));
+
+        Thread Transfer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    creditAccount.depositAmount(debitAccount, amount);
+                } catch (InsufficientBalanceException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Transfer.start();
+        notifyAboutTransfer(debitAccount,"DEBIT");
+        notifyAboutTransfer(creditAccount,"CREDIT");
+
+        log.info("Money Transfer Process completed Successfully");
+        return String.format("Money Transfer Successful");
+    }
+
+
+    private Long generateUniqueTransactionId() {
+        long val;
+        do {
+            final UUID uid = UUID.randomUUID();
+            final ByteBuffer buffer = ByteBuffer.wrap(new byte[16]);
+            buffer.putLong(uid.getLeastSignificantBits());
+            buffer.putLong(uid.getMostSignificantBits());
+            final BigInteger bi = new BigInteger(buffer.array());
+            val = bi.longValue();
+        }
+        // making sure that the ID is in positive space, if its not simply repeat the process
+        while (val < 0);
+        return val;
+    }
+
+    @Override
+    public void notifyAboutTransfer(Account account, String transferDescription) {
+        log.info(String.format("TransferDescription for %s : %s", account, transferDescription));
+    }
 }
